7c4d8b03d9d96d92c437d899c75e9f4f155b9c96
Amber-Eimer-Student <112406480+Amber-Eimer-Student@users.noreply.github.com>
2023-03-29 08:54:57 -0400

    finished Sliders and EndScreen video/project parts

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -30,11 +30,14 @@ public class Quiz : MonoBehaviour
 
     [Header("ProgressBar")]
     [SerializeField] Slider progressBar;
+    public bool isComplete;
 
     void Start()
     {
         timer = FindObjectOfType<Timer>();
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        progressBar.maxValue = questions.Count;
+        progressBar.value = 0;
     }
 
     void Update()
@@ -60,6 +63,10 @@ public class Quiz : MonoBehaviour
         SetButtonState(false);
         timer.CancelTimer();
         scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
+        if (progressBar.value == progressBar.maxValue)
+        {
+            isComplete = true;
+        }
     }
 
     void DisplayAnswer(int index)
@@ -91,6 +98,7 @@ public class Quiz : MonoBehaviour
             SetDefaultButtonSprites();
             GetRandomQuestion();
             DisplayQuestion();
+            progressBar.value++;
             scoreKeeper.IncrementQuestionsSeen();
         }
     }
